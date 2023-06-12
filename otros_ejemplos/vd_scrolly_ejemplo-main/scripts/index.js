60391008c519f8b8584b19e42a5358b3487c94dc
Pili Solari <psolaribarrios@mail.utdt.edu>
2023-06-11 22:55:18 -0300

    domingo1106

--- otros_ejemplos/vd_scrolly_ejemplo-main/scripts/index.js
+++ otros_ejemplos/vd_scrolly_ejemplo-main/scripts/index.js
@@ -11,7 +11,7 @@ let $step;
 let scroller = scrollama();
 
 // fetch data
-d3.csv("./data/JugadoresArgentina2022.csv", d3.autoType).then(function (data) {
+d3.csv("./data/top10tracksarg.csv", d3.autoType).then(function (data) {
   dataChart = data;
   // kick things off
   init();
@@ -26,6 +26,42 @@ function handleStepExit(response) {
 
 // scrollama event handlers
 function handleStepEnter(response) {
+
+  const nodos = dataChart
+
+  /*
+  // Dominios escalas
+  opacidad.domain(d3.extent(nodos, d => d.edad))
+  */
+  radio.domain(d3.extent(nodos, d => +d.popularidad))
+
+
+  /* DOM (cajiat) */
+  const svg = d3.select('body').append('svg').attr('width', width).attr('height', height)
+  const chart = svg
+    .append('g')
+    .attr('transform', `translate(${[width / 2, height / 2]})`)
+
+
+  const sim = d3
+    .forceSimulation(nodos)
+    .force('manybody', d3.forceManyBody().strength(200))
+    .force('center', d3.forceCenter())
+    .force('y', d3.forceY().strength(0.001))
+    .force(
+      'collide',
+      d3
+        .forceCollide(d => radio(+d.popularidad) + 5)
+        .strength(2)
+        .iterations(5),
+    )
+    /* Registra el observer */
+    sim.on('tick', redraw)
+    
+    /* Renderiza los círculos */
+    draw(chart, nodos)
+
+  /*
   // console.log(response);
   $step = d3.select(response.element);
 
@@ -45,6 +81,7 @@ function handleStepEnter(response) {
   // console.log("key", key);
 
   createChart(key);
+  */
 }
 
 function handleStepProgress(response) {
@@ -127,4 +164,38 @@ function createChart(key) {
 
   d3.select("#scrolly figure svg").remove();
   d3.select("#scrolly figure").append(() => chart);
+}
+
+function draw(chart, nodos) {
+  const tracks = chart
+    .selectAll('g.tracks')
+    .data(nodos)
+    .join('g')
+    .attr('class', 'tracks')
+    .attr('transform', d => `translate(${[d.x, d.y]})`)
+
+  tracks
+    .append('circle')
+    .attr('r', d => radio(+d.popularidad))
+    .style('stroke', (d, i) => color(d.danceability))
+    .style('fill', 'transparent')
+    .attr('stroke-width','2')
+    //.style('fill-opacity', d => opacidad(d.edad))
+  
+  tracks
+    .append('text')
+    .text(d => d.tema)
+    .attr('y', d => radio(+d.popularidad) / 9)
+    .attr('text-anchor', 'middle')
+    .style('fill','#00FFFF')
+    .attr('font-family','Poppins')
+    .attr('font-size', d => {
+      const size = radio(+d.popularidad) / 4
+      return size + 'px'
+    })
+  
+}
+
+function redraw() {
+  d3.selectAll('.tracks').attr('transform', d => `translate(${[d.x, d.y]})`)
 }
