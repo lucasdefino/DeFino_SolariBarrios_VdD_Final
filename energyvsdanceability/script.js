885a2fefe8a4ab4e36536068877db719882416c7
Pili Solari <psolaribarrios@mail.utdt.edu>
2023-06-16 17:18:46 -0300

    charts pop y danceenergy

---
+++ energyvsdanceability/script.js
@@ -0,0 +1,41 @@
+d3.csv('top10tracksargyesp.csv', d3.autoType).then(data => {
+  
+
+  let chart = Plot.plot({
+    width: 500,
+    height: 500,
+    marginLeft: 50,
+    marginRight: -40,
+    marginTop: 40,
+    marginBottom: 50,
+    color: {
+      range: ['#1400FF', '#D100D1'],
+    },
+    marks: [
+      Plot.dot(data, {
+        y: "enerfloat", 
+        x: "dancefloat",
+        r: 'popularidad',
+        opacity: 0.8,
+        fill: 'pais',
+
+      }),
+    ],
+    r: {range: [0,10]},
+    y: {
+      label: " energia",
+      ticks: [0.5,0.6,0.7,0.8],
+      grid: true,
+    },
+    x: {
+      label: "bailabilidad ",
+      ticks: [0.5,0.6,0.7,0.8],
+      grid: true,
+    },
+    style: {
+      backgroundColor: '#11001F',
+      color: '#00FFFF',
+    },
+    })
+  var svg = d3.select('#chart').append(() => chart)
+})
---
+++ popularidad/script.js
@@ -0,0 +1,47 @@
+d3.csv('top10tracksargyesp.csv', d3.autoType).then(data => {
+  
+  let maspopulares = data.filter(
+    d => d.popularidad >= 90,
+  )
+
+  let chart = Plot.plot({
+    width: 600,
+    height: 400,
+    marginLeft: 145,
+    color: {
+      range: ['#1400FF', '#D100D1'],
+    },
+    marks: [
+      Plot.barX(maspopulares, {
+        y: "tema", 
+        x: "popularidad",
+        fill: 'pais',
+        sort: {y: "-x"},
+        inset: 5,
+
+      }),
+      Plot.text(maspopulares,{
+        x: 'popularidad',
+        y: 'tema',
+        text: 'popularidad',
+        textAnchor: "end",
+        dx: -10,
+        fill: '#00FFFF',
+      })
+    ],
+    
+    y: {
+      label: null,
+      ticks: "",
+    },
+    x: {
+      label: null,
+      ticks: "",
+    },
+    style: {
+      backgroundColor: '#11001F',
+      color: '#00FFFF',
+    },
+    })
+  var svg = d3.select('#chart').append(() => chart)
+})
