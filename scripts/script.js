64a356f7b7caf961395fac8d92485dc87e9eb09d
lucasdefino <lucasdefino12@gmail.com>
2023-06-21 10:32:39 -0300

    funcionalidad boton

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
 function salto_poligono() {
-    d3.select('#poligonos').attr('src','poligonos_esp/index.html')
+    if (document.getElementById("salto_poligono").innerHTML === 'España'){
+        d3.select('#poligonos').attr('src','poligonos_esp/index.html')
+        d3.select('#salto_poligono').text('Argentina')
+    } else if (document.getElementById("salto_poligono").innerHTML === 'Argentina') {
+        d3.select('#poligonos').attr('src','poligonos_arg/index.html')
+        d3.select('#salto_poligono').text('España')
+    }
+
 }
